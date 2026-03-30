aebb8cf23a01aab8450f9f8960799a2cc7dd5835
Vamshi-chettamaina <vamshichettamaina@gmail.com>
2026-03-30 21:01:25 +0530

    forth commit

--- translator/views.py
+++ translator/views.py
@@ -1,14 +1,3 @@
-# from django.shortcuts import render
-# from deep_translator import GoogleTranslator
-
-# def translate_text(request):
-#     translated_text = ""
-#     if request.method == "POST":
-#         text = request.POST.get("text")
-#         dest = request.POST.get("dest")
-#         translated_text = GoogleTranslator(source='auto', target=dest).translate(text)
-
-#     return render(request, "translator.html", {"translated_text": translated_text})
 from deep_translator import GoogleTranslator
 from django.shortcuts import render
 
